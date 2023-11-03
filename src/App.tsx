3a8f82336f9f623fe761623d9093d50d602b437c
Aldo Exequiel Mleziva <aldo.exequiel.mleziva@hp.com>
2023-11-03 16:02:04 -0300

    added support for scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
+import viteLogo from './assets/vite.svg';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
