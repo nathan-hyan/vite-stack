b1ad10385a06662e1cdb6109c3cff7da21113fa7
Aldo Exequiel Mleziva <aldo.exequiel.mleziva@hp.com>
2023-11-03 12:49:29 -0300

    Added packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,71 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:jest-dom/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'warn',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@)(/.*|$)'],
+              // Absolute imports, starting with ~
+              ['^~.*(?:/|$)'],
+              // Parent imports. Put `..` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$', '^\\.\\.(?!/?$)'],
+              // Style imports.
+              ['^.+\\.?(scss|css)$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
-}
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'jest-dom',
+    'testing-library',
+    'simple-import-sort',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+
+};
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,23 +1,25 @@
-import {render, screen, waitFor} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import {describe, it, expect} from 'vitest'
+import { describe, expect, it } from 'vitest';
 
 import App from './App';
 
-describe("<App />", () => {
-  it("renders correctly", () => {
+describe('<App />', () => {
+  it('renders correctly', () => {
     render(<App />);
-    const title = screen.getByRole('heading', {level: 1})
-    expect(title).toHaveTextContent("Vite + React");
-  })
-    
-    it("increases count when clicked on button", async () => {
-        render(<App />);
+    const title = screen.getByRole('heading', { level: 1 });
+    expect(title).toHaveTextContent('Vite + React');
+  });
 
-        const button = screen.getByRole('button', { name: 'count is 0' });
+  it('increases count when clicked on button', async () => {
+    render(<App />);
 
-        await userEvent.click(button);
+    const button = screen.getByRole('button', { name: 'count is 0' });
 
-        waitFor(() => expect(button).toHaveTextContent('count is 1'));
-    })
-})
+    expect(button).not.toBeVisible();
+
+    await userEvent.click(button);
+
+    await waitFor(() => expect(button).toHaveTextContent('count is 1'));
+  });
+});
